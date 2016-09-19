9ae5a412531e26d5a15fa11352e8dde096d02112
Archie <archie@localhost.localdomain>
2016-09-19 22:18:02 +0200

    Rewrite script, improved subtitle extracting + burn-in

--- README.md
+++ README.md
@@ -7,24 +7,11 @@ As a bonus we'll also create a screenshot and a thumbnail for you.
 * Encode with ffmpeg, optimized for web (preset: ultrafast, tune: zerolatency, faststart)
 * Burn-in (extracted) subtitles-files into the video
 * Create a screenshot and thumbnail of the (encoded) video
+* Validate if encoding has been successfully done
 
-# FAQ
-## What packages need to be installed?
+# Readme
+## Requirements
+* [Linux](https://www.archlinux.org/)
 * [php](https://www.archlinux.org/packages/extra/x86_64/php/)
 * [ffmpeg](https://www.archlinux.org/packages/extra/x86_64/ffmpeg)
 * [imagemagick](https://www.archlinux.org/packages/extra/x86_64/imagemagick)
-
-## Why is this written in PHP?
-PHP(7) isn't that bad, and has many in-built functions.
-I agree ```shell_exec``` should be avoid, but I want to keep it asap.
-
-## Why no (full) video validation checking?
-Planned, but I assume you only want encode video files that have been tested.
-At the moment we simple do a file extension check.
-
-## Why encode mp4 files (again)?
-Usually the video isn't optimized for web (faststart), doesn't have a burned-in subtitle, etc.
-
-## Why burn-in subtitles?
-Their is still no (out-of-the-box) HTML5 solution for subtitles.
-It is however possible to simple change the ffmpeg command to your own needs.
--- encode.php
+++ encode.php
@@ -1,92 +1,118 @@
 <?php
-$base = '/home/archie/encode';
-$target = '/home/archie/video';
-$tmp = '/tmp';
-$encode = ['.mp4','.mkv','.wmv','.mpeg','.mpg','.avi','.ogm','.divx','.mov','.m4v','.flv'];
-$subs = ['ass','ssa','srt','sub'];
-$locales = ['eng','ned','en','nl','unk'];
+class Encode {
+  private $config = [
+    'base' => '/home/archie/encode/',
+    'target' => '/home/archie/encoded/',
+    'tmp' => '/tmp/conv/',
+    'locales' => ['eng','ned','en','nl','unk'],
+    'subtitles' => ['ass','ssa','srt','sub'],
+  ];
+  private $data = ['tmp' => [], 'encoded' => []];
 
-// functions
-function debug(string $str) {
-  echo date(DATE_RFC2822).": $str\n";
-}
+  public function __construct() {
+    // Create required directories
+    mkdir($this->config['tmp'], 0770);
+    mkdir($this->config['base'] . 'done', 0770);
 
-// Scan directory
-foreach (array_diff(scandir($base), array('..', '.')) as $key => $file) {
-  $name = strstr($file, '.', true);
-  $ext = strstr($file, '.');
+    // Scan directory
+    foreach (glob($this->config['base'].'*.{mp4,mkv,wmv,mpeg,mpeg,avi,ogm,divx,mov,m4v,flv}', GLOB_BRACE) as $file) {
+      // Set Data
+      $this->set('tmp', $file);
+      $this->set('encoded', $this->config['target'] . $this->get('tmp')['path']['filename'] . '.mp4');
 
-  // Needs to be encoded
-  if (!in_array($ext, $encode)) {
-    debug("Skip: $file");
-    continue;
+      // Is already encoded and valid?
+      $this->debug("Processing $file");
+      if (!$this->valid()) {
+        $this->debug('Encoding');
+        $this->extract()->encode();
+        continue;
+      }
+
+      // Final steps
+      $this->debug('Creating Images');
+      $this->images()->move();
+    }
   }
 
-  // Start!
-  debug("Processing $file");
+  public function set(string $key, string $path) {
+    $this->data[$key] = ['fullpath' => $path, 'path' => pathinfo($path), 'info' => $this->ffprobe($path)];
+  }
 
-  // Encode to mp4
-  if (!file_exists("$target/$name.mp4")) {
-    // Extract Subtitles
-    $data = json_decode(shell_exec("ffprobe -v quiet -print_format json -show_format -show_streams $file"), true);
-    if (!empty($data)) {
-      foreach ($data['streams'] as $subKey => $subVal) {
-        // No subtitle format is given
-        if (empty($subVal['codec_name']) && $subVal['codec_type'] == 'subtitle' || !empty($subVal['codec_name']) && stristr($subVal['codec_name'], 'subrip'))
-          $subVal['codec_name'] = 'srt';
+  public function debug(string $str) {
+    echo date(DATE_RFC2822)." $str\n";
+  }
 
-        // Check is subtitle stream
-        if (empty($subVal['codec_name']) || !in_array($subVal['codec_name'], $subs))
-          continue;
+  public function get(string $key) {
+    return $this->data[$key];
+  }
 
-        // When no language-tag has been given, select the first locale match
-        $subVal['tags'] = !empty($subVal['tags']) ? array_change_key_case($subVal['tags']) : []; // tags can be UPPERCASE
-        if (empty($subVal['tags']) || empty($subVal['tags']['language']))
-          $subVal['tags']['language'] = $locales[0];
+  public function ffprobe(string $path) {
+    return file_exists($path) ? json_decode(shell_exec("ffprobe -v quiet -print_format json -show_format -show_streams $path"), true) : [];
+  }
 
-        // Loop and extract on locale match
-        $path = "$base/$name.".$subVal['codec_name'];
-        foreach ($locales as $locale) {
-          if (stristr($subVal['tags']['language'], $locale) && !file_exists($path)) {
-            debug("Extract subtitle ($locale => $path)");
-            shell_exec("ffmpeg -v fatal -threads 0 -i $base/$file $path");
-          }
+  public function valid() {
+    if (!empty($this->get('encoded')['info'])) {
+      $original = $this->get('tmp')['info']['format']['duration'];
+      $encoded = $this->get('encoded')['info']['format']['duration'];
+      $size = 100 *(($encoded - $original) / $original);
+      $this->debug("Percentage diff: $size");
+      return ($size > -10 && $size < 10); // max. allowed margin
+    }
+    return false;
+  }
+
+  public function extract() {
+    foreach ($this->get('tmp')['info']['streams'] as $stream) {
+      // Skip non-subtitle stream
+      if ($stream['codec_type'] != 'subtitle')
+        continue;
+
+      // Validate codec
+      $format = strtolower($stream['codec_name']);
+      $format = in_array($format, $this->config['subtitles']) ? $format : 'srt';
+
+      // Strip tags
+      $stream['tags'] = is_array($stream['tags']) ? array_change_key_case($stream['tags']) : []; // Tags case can differ
+      $language = preg_replace('/[^a-zA-Z0-9]+/', '', $stream['tags']['language']) ?: 'unknown';
+
+      // Debug
+      $this->debug("Found subtitle ($language)");
+
+      // Extract subtitle
+      $path = $this->config['base'] . $this->get('tmp')['path']['filename'] . '_' . $stream['index'] . "$language.$format";
+      shell_exec(sprintf('ffmpeg -y -v fatal -threads 0 -i %s -map 0:%s %s', $this->get('tmp')['fullpath'], $stream['index'], $path));
+    }
+    return $this;
+  }
+
+  public function encode() {
+    $cmd = sprintf('ffmpeg -y -v fatal -threads 0 -i %s -c:v libx264 -crf 23 -preset ultrafast -c:a aac -b:a 160k -tune zerolatency -movflags +faststart', $this->get('tmp')['fullpath']);
+    foreach (glob(sprintf('%s_*.{%s}', $this->config['base'] . $this->get('tmp')['path']['filename'], implode(',', $this->config['subtitles'])), GLOB_BRACE) as $file) {
+      foreach ($this->config['locales'] as $locale) { // find first match
+        // Matches locale match and contains at least 150 lines (skip chapter-subtitle, etc.)
+        if (stristr($file, $locale) && shell_exec("wc -l < $file") > 150) {
+          $this->debug("Burn-In subtitle ($file)");
+          $cmd.= " -vf subtitles=$file";
+          break;
         }
       }
     }
-
-    // Add Subtitle (External)
-    $cmd = "ffmpeg -y -v fatal -threads 0 -i $base/$file -c:v libx264 -crf 23 -preset ultrafast -tune zerolatency -movflags +faststart";
-    foreach ($subs as $sub) {
-			$path = "$base/$name.$sub";
-			if (file_exists($path)) {
-        $cmd.= " -vf subtitles=$path";
-        debug("Burn-In Subtitle ($path)");
-				break;
-      }
-		}
-
-    // Start Encode
-    debug("Start Encoding ($cmd)");
-    shell_exec("$cmd $target/$name.mp4");
-    debug("Encoding finished ($target/$name.mp4)");
+    shell_exec(sprintf("$cmd %s", $this->get('encoded')['fullpath']));
+    return $this;
   }
 
-  // When Encoded
-  elseif (file_exists("$target/$name.mp4")) {
-    // Create Thumb and screen
-    if (!file_exists("$target/$name-thumb.jpg") || !file_exists("$target/$name-screen.jpg")) {
-      debug("Create thumb and screen");
-      shell_exec("mkdir -p $tmp/conv; rm -rf $tmp/conv/*;
-                  ffmpeg -threads 0 -v fatal -i $target/$name.mp4 -an -s 480x300 -vf fps=1/100 -qscale:v 10 $tmp/conv/1-%03d.jpg;
-                  montage -mode concatenate -tile 4x $tmp/conv/1-*.jpg $target/$name-screen.jpg;
-                  cp -f $tmp/conv/1-001.jpg $target/$name-thumb.jpg; cp -f $tmp/conv/1-002.jpg $target/$name-thumb.jpg");
-    }
+  public function images() {
+    $cmd = 'rm -rf %tmp%*; ffmpeg -threads 0 -v fatal -i %target%.mp4 -an -s 480x300 -vf fps=1/100 -qscale:v 10 %tmp%1-%03d.jpg;';
+    $cmd.= 'cp -f %tmp%1-001.jpg %target%-thumb.jpg; cp -f %tmp%1-002.jpg %target%-thumb.jpg;';
+    $cmd.= 'montage -mode concatenate -tile 4x %tmp%1-*.jpg %target%-screen.jpg';
+    shell_exec(str_replace(['%tmp%','%target%'], [$this->config['tmp'], $this->config['target'] . $this->get('tmp')['path']['filename']], $cmd));
+    return $this;
+  }
 
-    // Move to complete-dir
-    else {
-      shell_exec("mkdir -p $base/done; mv $base/$name.* $base/done");
-      debug("Done: $base/$name.* => $base/done");
-    }
+  public function move() {
+    shell_exec(str_replace(['%keep%','%filename%'], [$this->config['base'] . 'done', $this->config['base'] . $this->get('tmp')['path']['filename']], 'mv -t %keep% %filename%.* %filename%_*'));
+    return $this;
   }
 }
+
+new Encode();
