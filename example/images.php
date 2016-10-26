c5881b5e36d7a666b2cc1964411c3520f54a0c57
François M <francoism90@users.noreply.github.com>
2016-10-26 20:38:50 +0200

    Update images.php

--- example/images.php
+++ example/images.php
@@ -1,6 +1,14 @@
 <?php
 require('../ffmpeg.php');
 
-$ffmpeg = new FFmpeg('/mnt/data/convert/', '/mnt/data/convert/');
-$ffmpeg->images(['threads' => 4])
-       ->start();
+// Init
+$ffmpeg = new FFmpeg(['output' => '/home/archie/Videos/Images/', 'hwaccel' => 'intel_h264_vaapi']);
+
+// Scan
+foreach (glob('/home/archie/Videos/' . '*.{avi,divx,flv,m4v,mkv,mov,mp4,mpeg,mpg,ogm,wmv}', GLOB_BRACE) as $file) {
+  // Show file
+  echo "Processing $file\n";
+
+  // Exec encoding
+  echo date('d-m-Y @ H:i:s') . ": Creating images\n";
+}
